b01e1c4916a784552b6d73cc1973e968daa15c74
Tristan Slater <hello@trslater.ca>
2020-12-09 01:40:35 -0800

    Make fizzbuzz configurable

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,8 @@
 import sys
 
+from itertools import count, islice
+from math import ceil
+
 
 def main(argv):
     try:
@@ -13,12 +16,16 @@ def main(argv):
         print("Error: argument must be an integer")
         sys.exit(1)
 
-    fizzbuzz(n)
+    f = configurable({"fizz": 3, "buzz": 5, "jazz": 7})
+
+    for _ in range(n):
+        print(next(f))
 
 
-def fizzbuzz(n):
-    for i in range(1, n+1):
-        print(("fizz" if i%3 == 0 else "") + ("buzz" if i%5 == 0 else "") or i)
+def configurable(config):
+    return (
+        "".join(msg for msg,freq in config.items() if i%freq == 0) or i
+        for i in count(1))
 
 
 if __name__ == "__main__":
